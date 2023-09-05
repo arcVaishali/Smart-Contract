c5093b33bcc459085fca3a91db0f8fb6f3504ce7
Vaishali <98754461+arcVaishali@users.noreply.github.com>
2023-09-05 18:37:37 +0530

    Update send-and-mint-coins.sol

--- send-and-mint-coins.sol
+++ send-and-mint-coins.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract yourToken {
+    address public minter;
+
+    mapping ( address => uint ) public balances;
+
+    event Sent( address from, address to, uint amount) ;
+    function yourtoken() public {
+        minter = msg.sender ;
+    }
+    function mint( address receiver , uint amount ) public{
+        if ( msg.sender != minter ) return ;
+        balances[ receiver ] += amount ;
+    } 
+    function send( address receiver , uint amount) public {
+        if ( balances[ msg.sender ] < amount ) return ;
+        balances[ msg.sender]-=amount ;
+        balances[ receiver ] += amount ;
+        emit Sent( msg.sender , receiver , amount );
+    }
+}
+
+
 
